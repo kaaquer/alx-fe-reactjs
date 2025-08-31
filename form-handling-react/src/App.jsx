dd59b7e3d63edfc6789528f2d99a5591c48fc717
kaaquer <kaaquer2005@gamil.com>
2025-08-31 22:11:10 +0000

    task 0

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import RegistrationForm from './components/RegistrationForm';
-import FormikForm from './components/formikForm';
+import FormikForm from './components/formikForm.jsx';
 import './App.css';
 
 function App() {
